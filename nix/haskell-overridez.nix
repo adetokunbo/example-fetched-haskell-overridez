1a6491c734891ed944d98c80ffe5a83d5e73de40
Tim Emiola <adetokunbo@users.noreply.github.com>
2018-05-20 09:16:57 +0900

    Updated to the newer version haskell-overridez

--- nix/haskell-overridez.nix
+++ nix/haskell-overridez.nix
@@ -2,7 +2,7 @@ let
   pkgs = import <nixpkgs> {};
   overridez = fetchTarball {
     url = "https://github.com/adetokunbo/haskell-overridez/archive/master.tar.gz";
-    sha256 = "0w3akza30rbayz23fyjlgh9w1vfhb4jxfb2vrn4ssanha9xsaaqy";
+    sha256 = "03a3fi1398ms0lbrmljphhgh4b6mlb80hw8ilfqdpjbg303z5cp1";
   };
 in
   import overridez { inherit pkgs; }
